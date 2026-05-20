bfd617c626a385205c5e182b04ec8e84d50626b1
Felipeacs05 <felipedocarmo05@gmail.com>
2026-05-20 17:59:06 -0300

    feat: implement MyTripsPage and trips list display

--- src/app/pages/my-trips/my-trips.page.ts
+++ src/app/pages/my-trips/my-trips.page.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService, Trip } from '../../services/data.service';
 
+/**
+ * Page component representing the My Trips list.
+ * Displays all past and current trips loaded from local storage.
+ * 
+ * @author Antigravity
+ */
 @Component({
   selector: 'app-my-trips',
   templateUrl: './my-trips.page.html',
   styleUrls: ['./my-trips.page.scss'],
 })
 export class MyTripsPage implements OnInit {
+  trips: Trip[] = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.loadTrips();
   }
 
+  async ionViewWillEnter() {
+    await this.loadTrips();
+  }
+
+  /**
+   * Fetches the current list of trips from DataService.
+   */
+  async loadTrips() {
+    this.trips = await this.dataService.getTrips();
+  }
+
+  /**
+   * Helper to generate dummy arrays for star rendering.
+   */
+  getStars(rating: number): number[] {
+    return Array(rating);
+  }
+
+  getEmptyStars(rating: number): number[] {
+    return Array(5 - rating);
+  }
 }
