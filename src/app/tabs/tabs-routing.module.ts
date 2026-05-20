06eaebe1491117c75d36d6796cd7015d3042779c
Felipeacs05 <felipedocarmo05@gmail.com>
2026-05-20 12:45:21 -0300

    feat: generate tabs and main pages routing

---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'map',
+        loadChildren: () => import('../pages/map/map.module').then(m => m.MapPageModule)
+      },
+      {
+        path: 'my-trips',
+        loadChildren: () => import('../pages/my-trips/my-trips.module').then(m => m.MyTripsPageModule)
+      },
+      {
+        path: 'finances',
+        loadChildren: () => import('../pages/finances/finances.module').then(m => m.FinancesPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../pages/profile/profile.module').then(m => m.ProfilePageModule)
+      },
+      {
+        path: '',
+        redirectTo: 'map',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
